5baed9c4bd63e4ef65983db7ebfa93cb4d323994
poonamdent <poonammore.dent@gmail.com>
2025-12-03 13:17:21 +0530

    Full project

---
+++ Pages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ColdStorage.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
